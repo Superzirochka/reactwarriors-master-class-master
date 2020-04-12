0a10040d52126b8d56ebf1b0fe2d6cd65dfeeda4
Superzirochka <SUPERZIROCHKA@GMAIL.COM>
2020-04-12 16:44:29 +0300

    Updates

---
+++ precache-manifest.26a21df9215217f38f573f94a01f57f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "238bdc4318913c13e692",
+    "url": "/reactwarriors-master-class-master/static/js/main.be962a39.chunk.js"
+  },
+  {
+    "revision": "562f228c0becea4d12de",
+    "url": "/reactwarriors-master-class-master/static/js/runtime~main.6397383a.js"
+  },
+  {
+    "revision": "47486bce1f141e81e3dc",
+    "url": "/reactwarriors-master-class-master/static/css/2.2f2d4aa3.chunk.css"
+  },
+  {
+    "revision": "47486bce1f141e81e3dc",
+    "url": "/reactwarriors-master-class-master/static/js/2.5ba95119.chunk.js"
+  },
+  {
+    "revision": "08924f2cf9a850b5ab265cb5cb18201c",
+    "url": "/reactwarriors-master-class-master/index.html"
+  }
+];
